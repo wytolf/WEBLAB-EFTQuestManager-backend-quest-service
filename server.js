bb668b50d52443103a6bc48d55279f7f6017dc1b
Matthias Brunner <brunner.matthias@outlook.com>
2024-03-22 17:51:51 +0100

    refactor: add loggerframework (winston) to backend services

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const winston = require('winston');
 
 const initializeApp = require('firebase/app');
 const {getFirestore, collection, getDocs, doc, getDoc, setDoc} = require('firebase/firestore');
@@ -14,35 +15,47 @@ const firebaseConfig = {
 };
 
 function main() {
-
     const server = express();
     server.use(express.json())
 
     const app = initializeApp.initializeApp(firebaseConfig);
     const db = getFirestore(app);
+    const logger = winston.createLogger({
+        level: 'info',
+        format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.timestamp(),
+            winston.format.align(),
+            winston.format.printf(info => `${info.timestamp} ${info.service} ${info.level}: ${info.message}`)
+        ),
+        defaultMeta: {service: 'quest-service'},
+        transports: [
+            new winston.transports.Console(),
+        ],
+    });
 
     server.listen(process.env.PORT, () => {
-        console.log('Quest Server is listening....');
+        logger.info('Server is listening');
     });
 
     server.get('/api/quests', async (req, res) => {
-        console.log(`Quest Service: GET /api/quests wurde aufgerufen.`);
+        logger.info(`GET /api/quests wurde aufgerufen.`);
 
         try {
             const questList = await getAllQuests(db);
             res.status(200).json(questList);
-            console.log(`Quest Service: GET /api/quests -> Daten erfolgreich gesendet`);
+            logger.info(`GET /api/quests -> Daten erfolgreich gesendet`);
         } catch (error) {
-            console.error('Quest Service: GET /api/quests -> Error fetching quests:', error);
-            res.status(500).send('Quest Service: GET /api/quests -> Internal Server Error');
+            logger.error('GET /api/quests -> Error fetching quests:', error);
+            res.status(500).send('GET /api/quests -> Internal Server Error');
         }
     });
 
     server.post('/api/quests', async (req, res) => {
-        console.log(`Quest Service: POST /api/quests wurde aufgerufen.`);
+        logger.info(`POST /api/quests wurde aufgerufen.`);
 
         const {title, map, trader, link} = req.body;
-        console.log('Quest Service: POST /api/quests -> Request received { title: ' + title + ' }');
+        logger.info('POST /api/quests -> Request received { title: ' + title + ' }');
         try {
             await setDoc(doc(db, "quests", title), {
                 link: link,
@@ -50,18 +63,18 @@ function main() {
                 title: title,
                 trader: trader
             });
-            console.log('Quest Service: POST /api/quests -> Quest erfolgreich hinzugefügt. { title: ' + title + ' }');
+            logger.info('POST /api/quests -> Quest erfolgreich hinzugefügt. { title: ' + title + ' }');
             res.status(200).send();
         } catch (error) {
-            console.error('Quest Service: GET /api/quests -> Error fetching quests:', error);
-            res.status(500).send('Quest Service: GET /api/quests -> Internal Server Error');
+            logger.error('GET /api/quests -> Error fetching quests:', error);
+            res.status(500).send('GET /api/quests -> Internal Server Error');
         }
 
     });
 
     server.get('/api/quests/:id', async (req, res) => {
         const id = req.params.id;
-        console.log(`Quest Service: GET /api/quests/` + id + ` wurde aufgerufen.`);
+        logger.info(`GET /api/quests/` + id + ` wurde aufgerufen.`);
 
         try {
             const questDocument = doc(db, "quests", id);
@@ -70,27 +83,26 @@ function main() {
 
             if (questSnapshot.exists()) {
                 let quest = questSnapshot.data();
-                console.log(`Quest Service: GET /quests/` + id + ` -> quest gefunden` + quest);
+                logger.info(`GET /quests/` + id + ` -> quest gefunden` + quest);
                 res.status(200).json(quest);
-                console.log(`Quest Service: GET /quests/` + id + ` -> Daten erfolgreich gesendet`);
+                logger.info(`GET /quests/` + id + ` -> Daten erfolgreich gesendet`);
             } else {
-                console.log("Quest Service: GET /quests/" + id + " -> Document existiert nicht in db");
-                res.status(404).send("Quest Service: GET /quests/" + id + ' -> Quest nicht gefunden');
+                logger.info("GET /quests/" + id + " -> Document existiert nicht in db");
+                res.status(404).send("GET /quests/" + id + ' -> Quest nicht gefunden');
             }
         } catch
             (error) {
-            console.error('Quest Service: GET /api/quests/' + id + ' -> Error fetching quest:', error);
-            res.status(500).send('Quest Service: GET /api/quests ' + id + ' -> Error fetching quest:' + error);
+            logger.error('Quest Service: GET /api/quests/' + id + ' -> Error fetching quest:', error);
+            res.status(500).send('GET /api/quests ' + id + ' -> Error fetching quest:' + error);
         }
     });
+
+    async function getAllQuests(db) {
+        const quests = collection(db, 'quests');
+        const questsSnapshot = await getDocs(quests);
+        return  questsSnapshot.docs.map(doc => doc.data());
+    }
 }
 
-async function getAllQuests(db) {
-    const quests = collection(db, 'quests');
-    const questsSnapshot = await getDocs(quests);
-    const allQuests = questsSnapshot.docs.map(doc => doc.data());
-    console.log(`Quest Service: GET /api/quests -> QuestList: ${allQuests}`);
-    return allQuests;
-}
 
 main();
